84338b81469266f72067318b700fe9f7d5326074
tester313243 <4belstu@mail.ru>
2022-03-13 14:06:36 +0300

    first lab

---
+++ LAB_1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Калькулятор
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label1.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(100, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ввведите первое число";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(103, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label2.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(334, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ввведите второе число";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(337, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 34);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label3.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(100, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Выберите операцию";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/",
+            "/-",
+            "/+"});
+            this.comboBox1.Location = new System.Drawing.Point(103, 234);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 37);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(103, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(362, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 41);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(103, 378);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(382, 34);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.label4.Font = new System.Drawing.Font("News706 BT", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(243, 337);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Результат";
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(524, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 41);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Вернуть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(667, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.Text = "Простой калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LAB_1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Калькулятор
+{
+ /*   public static partial class Extensions
+    {
+
+        public static Boolean IsDigit(this String s, Int32 index)
+        {
+            return Char.IsDigit(s, index);
+        }
+    }*/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+            double a, b;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+
+            switch (comboBox1.Text)
+            {
+                case "+":
+                    double res1 = a + b;
+                    int finalres1 = (int)res1;
+                    //запись
+                    string text = Convert.ToString(finalres1);
+                    File.WriteAllText(@"D:\University\БГТУ\2 Курс\4. Семместр\ООП\All Lab\LAB_1\bin\Debug\info.txt", "");//каждый раз новый текст
+                    using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+                    {
+
+
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text);//запись в массив
+                        stream.Write(array, 0, array.Length);//массив отступ длинна
+
+
+                    }
+
+                    //вывод
+                    textBox3.Text = Convert.ToString(finalres1); break;
+                    
+
+                case "-":
+                    double res2 = a - b;
+                    int finalres2 = (int)res2;
+                    //запись
+                    string text2 = Convert.ToString(finalres2);
+                    File.WriteAllText(@"D:\University\БГТУ\2 Курс\4. Семместр\ООП\All Lab\LAB_1\bin\Debug\info.txt", "");//каждый раз новый текст
+                    using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+                    {
+
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text2);//запись в массив
+                        stream.Write(array, 0, array.Length);//массив отступ длинна
+
+                    }
+                    //вывод
+                    textBox3.Text = Convert.ToString(finalres2); break;
+                case "*":
+                    double res3 = a * b;
+                    int finalres3 = (int)res3;
+                    //запись
+                    string text3 = Convert.ToString(finalres3);
+                    File.WriteAllText(@"D:\University\БГТУ\2 Курс\4. Семместр\ООП\All Lab\LAB_1\bin\Debug\info.txt", "");//каждый раз новый текст
+                    using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+                    {
+
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text3);//запись в массив
+                        stream.Write(array, 0, array.Length);//массив отступ длинна
+
+                    }
+                    //вывод
+                    textBox3.Text = Convert.ToString(finalres3); break;
+                case "/":
+                    if (b==0)
+                    {
+                        MessageBox.Show("На ноль делить нельзя");
+                    }
+                    double res4 = a / b;
+                    int finalres4 = (int)res4;
+                    //запись
+                    string text4 = Convert.ToString(finalres4);
+                    File.WriteAllText(@"D:\University\БГТУ\2 Курс\4. Семместр\ООП\All Lab\LAB_1\bin\Debug\info.txt", "");//каждый раз новый текст
+                    using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+                    {
+
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text4);//запись в массив
+                        stream.Write(array, 0, array.Length);//массив отступ длинна
+
+                    }
+                    //вывод
+                    textBox3.Text = Convert.ToString(finalres4); break;
+                case "/-":
+                    /* char fina = '8';
+                     string result = Convert.ToString(a / b);
+
+                     for (int i = 0; i < result.Length; i++)
+                     {
+                         if (!result.IsDigit(i))
+                         {
+                             i++;
+                             fina = result[i];
+                         }
+                     }
+                     textBox3.Text = Convert.ToString(fina); break;*/
+                    double res = a / b;
+                    string result = Convert.ToString(res);
+                    string[] finalres = result.Split(new char[] { ',' });
+                    //запись
+                    string text5 = Convert.ToString(finalres[1]);
+                    File.WriteAllText(@"D:\University\БГТУ\2 Курс\4. Семместр\ООП\All Lab\LAB_1\bin\Debug\info.txt", "");//каждый раз новый текст
+                    using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+                    {
+
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text5);//запись в массив
+                        stream.Write(array, 0, array.Length);//массив отступ длинна
+
+                    }
+                    //вывод
+                    textBox3.Text = Convert.ToString(finalres[1]); break;
+
+                case "/+":
+                    double o = a / b;
+                    int intresult = (int)o;
+                    //запись
+                    string text6 = Convert.ToString(intresult);
+                    File.WriteAllText(@"D:\University\БГТУ\2 Курс\4. Семместр\ООП\All Lab\LAB_1\bin\Debug\info.txt", "");//каждый раз новый текст
+                    using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+                    {
+
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text6);//запись в массив
+                        stream.Write(array, 0, array.Length);//массив отступ длинна
+
+                    }
+                    //вывод
+
+                    textBox3.Text = Convert.ToString(intresult); break;
+                default:
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            comboBox1.Text = "";
+        }
+
+   
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FileStream stream1 = File.OpenRead("info.txt"))
+            {
+                byte[] array = new byte[stream1.Length];
+                stream1.Read(array, 0, array.Length);
+
+                string textFromFile = Encoding.Default.GetString(array);
+                textBox3.Text = textFromFile;
+            }
+
+        }
+    }
+}
