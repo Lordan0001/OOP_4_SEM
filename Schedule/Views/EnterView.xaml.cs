ab048b503732295c22f8c5206d687e403afd0420
tester313243 <4belstu@mail.ru>
2022-05-08 23:28:17 +0300

    MVVM + Vallidation

---
+++ Schedule/Views/EnterView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwitchingViewsMVVM.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterView.xaml
+    /// </summary>
+    public partial class EnterView : UserControl
+    {
+        public EnterView()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (log.Text == "Admin" && pass.Text == "1111")
+            {
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                //this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Проверьте пароль или логин");
+            }
+        }
+    }
+}
